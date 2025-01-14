1e9e08c49f6de84b07e842904aca82783e351005
DESKTOP-FUQ1C3A\HOME <binho.bgu@gmail.com>
2025-01-14 17:52:18 -0300

    ajuste nos links e telas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "fundo-site": "url('./assets/images/sala_atendimento2.jpg')",
+      },
+    },
   },
-  plugins: [],
-}
 
+  plugins: [],
+};
